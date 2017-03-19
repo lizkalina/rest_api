2aab68ad2036b857661416e435ca40734d7ab998
lizkalina <liz.kalina@gmail.com>
2017-03-19 19:33:13 -0400

    API fix

--- api/people/urls.py
+++ api/people/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from .views import PersonDetail, PersonList
 
 api_patterns = [
-    url(r'^people/(?P<pk>[0-9]+)\/?$', PersonDetail.as_view()),
-    url(r'^people/', PersonList.as_view()),
+    url(r'people/(?P<pk>[0-9]+)\/?$', PersonDetail.as_view()),
+    url(r'people/', PersonList.as_view()),
     ]
