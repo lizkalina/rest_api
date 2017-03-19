1c0f7b3ca24eeb4fe005292ee50b0565fcaa4f03
lizkalina <liz.kalina@gmail.com>
2017-03-19 18:44:20 -0400

    Fix typo in settings

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
