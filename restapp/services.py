d03c4aa6a87aecfa0aa762b169eb9ec1a20bd444
lizkalina <liz.kalina@gmail.com>
2017-03-19 10:28:04 -0400

    Render to template configuration

--- restapp/services.py
+++ restapp/services.py
@@ -1,30 +1,29 @@
 import requests
+import json
 from helpers import _url
 import pdb
 
 class PersonService:
 
-    def __init__(self,pk=None,name=None,favorite_city=None):
-        self.pk = pk
-        self.name = name
-        self.favorite_city = favorite_city
-
     def get_people(self):
-        return requests.get(_url('/people/'))
+        response = requests.get(_url('/people/'))
+        return json.loads(response.text)
 
-    def get_person(self):
-        return requests.get(_url('/people/{}/'.format(self.pk)))
+    def get_person(self,pk):
+        response = requests.get(_url('/people/{}/'.format(pk)))
+        pdb.set_trace()
+        return json.loads(response.text)
 
-    def create_person(self):
+    def create_person(self,name,favorite_city):
         return requests.post(_url('/people/'), json={
-            'name': self.name,
-            'favorite_city': self.favorite_city,
+            'name': name,
+            'favorite_city': favorite_city,
             })
 
-    def delete_person(self):
-        return requests.delete(_url('/people/{}/'.format(self.pk)))
+    def delete_person(self,pk):
+        return requests.delete(_url('/people/{}/'.format(pk)))
 
-    def update_person(self):
+    def update_person(self,pk,name,favorite_city):
         url = _url('/people/{}/'.format(self.pk))
         return requests.put(url, json={
             'name': self.name,
--- restapp/views.py
+++ restapp/views.py
@@ -8,12 +8,11 @@ import pdb
 def index(request):
     api_call = PersonService()
     person_list = api_call.get_people()
-    context = {'person_list': person_list}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'person_list': person_list})
 
 def detail(request, pk):
-    api_call = PersonService(pk=pk)
-    person = api_call.get_person()
+    api_call = PersonService()
+    person = api_call.get_person(pk)
     return render(request, 'person/detail.html', {'person': person})
 
 
@@ -25,10 +24,10 @@ def create(request, name, favorite_city):
 
 def update(request, pk):
     api_call = PersonService()
-    person = api_call.get_person(pk=pk)
+    person = api_call.update_person(pk=pk,name=name,favorite_city=favorite_city)
     return render(request, 'person/detail.html', {'person': person})
 
 def delete(request, pk):
     api_call = PersonService()
-    person = api_call.get_person(pk=pk)
+    person = api_call.delete_person(pk=pk)
     return render(request, 'person/detail.html', {'person': person})
